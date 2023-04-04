24777c143d644a00309f0d50d4d3ae5601a02b25
Your Name <you@example.com>
2023-04-04 16:26:10 -0400

    New stuff

--- rl3.py
+++ rl3.py
@@ -2,11 +2,25 @@ import time
 import gym
 import numpy as np
 
-env = gym.make("CartPole-v1", render_mode='human')
-
+# env = gym.make("CartPole-v1", render_mode='human')
+env = gym.make("CartPole-v1")
+print(env.action_space.n)
 print("Observation Space: ", env.observation_space)
 print("Action Space       ", env.action_space)
 
+def create_bin_edges(ranges, num_of_bins):
+    return np.array([np.linspace(ranges[0][0], ranges[0][1], num_of_bins + 1),
+                         np.linspace(ranges[1][0], ranges[1][1], num_of_bins + 1),
+                         np.linspace(ranges[2][0], ranges[2][1], num_of_bins + 1),
+                         np.linspace(ranges[3][0], ranges[3][1], num_of_bins + 1)])
+
+def discretize_state2(state):
+    return [
+        np.digitize(state[0], bin_edges[0]) - 1,
+        np.digitize(state[1], bin_edges[1]) - 1,
+        np.digitize(state[2], bin_edges[2]) - 1,
+        np.digitize(state[3], bin_edges[3]) - 1
+    ]
 
 def discretize_state(state, ranges, num_of_intervals):
     # ranges will be formatted like: [[-4.8, 4.8], [-5, 5], [-0.21, 0.21], [-5, 5]]
@@ -35,6 +49,7 @@ def e_greedy_policy(state, epsilon):
     chosen_action = None
     max_action = None
     max_val = float('-inf')
+    
     for action, q_val in enumerate(q_table[tuple(state)]):
         if q_val > max_val:
             max_action = action
@@ -60,32 +75,47 @@ def max_action(state):
     return max_action
 
 if __name__ == "__main__":
-    q_table = np.zeros([10, 10, 10, 10, 2])
-    ranges = [[-4.8, 4.8], [-5, 5], [-0.21, 0.21], [-5, 5]]
-    num_of_intervals = 10
+    q_table = np.random.uniform(low=0, high=1, size=([30, 30, 30, 30] + [env.action_space.n]))
+    # q_table = np.zeros([40, 40, 40, 40, 2])
+    ranges = [[-2.4, 2.4], [-5, 5], [-0.21, 0.21], [-5, 5]]
+    num_of_intervals = 40
     epsilon = 0.9
-    gamma = 0.5
-    step_size = 0.5
-    obs = env.reset()
-    last_state = discretize_state(obs[0], ranges, num_of_intervals)
-    done = False
-    for i in range(10):
+    gamma = 0.9
+    num_of_bins = 30
+    bin_edges = create_bin_edges(ranges, num_of_bins)
+    print(bin_edges[0])
+    step_size = 0.1
+    average_reward = 0
+    total_reward = 0
+    for i in range(1000):
+        if i % 50 == 0:
+            average_reward = total_reward / 50
+            print(f"Average reward for past 50 episodes: {average_reward}")
+            total_reward = 0
+        obs = env.reset()
+        done = False
+        # last_state = discretize_state(obs[0], ranges, num_of_intervals)
+        last_state = discretize_state2(obs[0])
         while not done:
             # action = env.action_space.sample()
             last_action = e_greedy_policy(last_state, epsilon)
-            print(f"last_state: {last_state}")
-            print(f"obs: {obs}")
             obs, reward, done, truncated, info = env.step(last_action)
-            print(f"done: {done}, reward: {reward}")
             if not done:
-                current_state = discretize_state(obs, ranges, num_of_intervals)
+                # current_state = discretize_state(obs, ranges, num_of_intervals)
+                current_state = discretize_state2(obs)
                 last_state_action_value = q_table[tuple(last_state), last_action]
-                max_next_state_action_value = q_table[tuple(current_state), max_action(current_state)]
-                print(f"reward: {reward}")
-                q_table[last_state, last_action] += step_size * (reward + gamma * max_next_state_action_value - last_state_action_value)
+                max_next_state_action_value = np.max(q_table[tuple(current_state)])
+                # q_table[last_state, last_action] += step_size * (reward + gamma * max_next_state_action_value - last_state_action_value)
+                q_table[last_state, last_action] = (1 - step_size) * q_table[last_state, last_action] + (reward + gamma * max_next_state_action_value)
                 # print(f"Discretized state: {discretize_state(obs, ranges, num_of_intervals)}")
                 # print(reward)
                 last_state = current_state
-                env.render()
+                total_reward += 1
+                # env.render()
                 time.sleep(0.01)
+            else:
+                # q_table[last_state, last_action] += step_size * (-10 + gamma * max_next_state_action_value - last_state_action_value)
+                q_table[last_state, last_action] = (1 - step_size) * q_table[last_state, last_action] + (-10)
+        epsilon *= 0.99
+                
     env.close()
