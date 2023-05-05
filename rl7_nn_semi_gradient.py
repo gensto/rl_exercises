3896afc39c7c8eb9788c6182f643a00f921085e5
Your Name <you@example.com>
2023-05-05 16:06:27 -0400

    New stuff

--- rl7_nn_semi_gradient.py
+++ rl7_nn_semi_gradient.py
@@ -1,24 +1,68 @@
 import numpy as np
 
 def relu(inputs):
+    inputs[0][inputs[0] < 0] = 0
     
+    return inputs
 
 def forward_prop(state):
-    feature_vector = np.zeros((500, 1))
+    feature_vector = np.zeros((50, 1))
     feature_vector[state][0] = 1
+
+    return np.dot(relu(np.dot(np.transpose(feature_vector), weights[0]["W"])), weights[1]["W"])
+
+def get_grads(state):
+    feature_vector = np.zeros((50, 1))
+    feature_vector[state][0] = 1
+    state_value = forward_prop(state)[0, 0]
     
-    return relu(np.transpose(feature_vector) * weights[0]["W"]) * weights[1]["W"]
+    return np.array([
+        np.dot(feature_vector, np.transpose(weights[1]["W"])),
+        np.dot(relu(np.transpose(weights[0]["W"])), feature_vector)
+    ])
+
+def get_state_values():
+    for i in range(50):
+        print(f"State {i} value: {forward_prop(i)[0,0]}")
+    
 
 if __name__ == "__main__":
     weights = [
         {
-            "W": np.zeros((500,400))
+            "W": np.ones((50,40))
         },
         {
-            "W": np.zeros((400, 1))
+            "W": np.ones((40, 1))
         }
     ]
-
+    
+    step_size = 0.01
+    gamma = 0.7
+    num_of_episodes = 1000
+    
+    for n in range(num_of_episodes):
+        reward = 0
+        done = False
+        current_state = 25
+        
+        while not done:
+            action = np.random.choice([-1, 1])
+            new_state = current_state + action
+            reward = 0
+            if new_state == 0:
+                done = True
+                reward = -10
+            elif new_state == 49:
+                done = True
+                reward = 10
+            td_error = reward + gamma * forward_prop(new_state)[0, 0] - forward_prop(current_state)[0, 0]
+            grads = get_grads(current_state)
+            weights[0]["W"] += step_size * td_error * grads[0]
+            weights[1]["W"] += step_size * td_error * grads[1]
+            
+            current_state = new_state
+    
+    get_state_values()           
 
 # def state_info(state):
 #     num_of_rows = 4
