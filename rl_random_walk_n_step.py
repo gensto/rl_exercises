39bde7aa25ba59cdac8732040d8f4e8078fc80c8
Your Name <you@example.com>
2023-05-09 16:19:49 -0400

    New stuff

--- rl_random_walk_n_step.py
+++ rl_random_walk_n_step.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     state_and_rewards_received = []
     
     num_of_episodes = 50
+    gamma = 0.5
     
     weights = [
         {
@@ -26,13 +27,17 @@ if __name__ == "__main__":
         while t < T:
             action = np.random.choice([-1, 1])
             next_state = current_state + action
+            reward = 0
             if next_state == state_space_size - 1:
                 T = t + 1
                 reward = 10
             elif next_state == 0:
                 T = t + 1
                 reward = -10
+            state_and_rewards_received.append((reward, next_state))
             tau = t - n + 1
-        if tau >= 0:
-            G = 
-    
+            if tau >= 0:
+                G = 0
+                i  = tau + 1
+                while i < range(min(tau + n, T)):
+                    G += pow(gamma, i - tau - 1) * 
