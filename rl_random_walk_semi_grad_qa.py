75dcbb6b5392a67ad457d27f87a9cfd7c276431c
Your Name <you@example.com>
2023-05-18 16:16:44 -0400

    New stuff

--- rl_random_walk_semi_grad_qa.py
+++ rl_random_walk_semi_grad_qa.py
@@ -7,6 +7,16 @@ def relu(inputs):
     
     return inputs
 
+def get_grads(state, action):
+    feature_vector = np.zeros((1, state_space_size))
+    feature_vector[0][state] = 1
+    w1_grads = weights[1]["W"][:, action] * feature_vector
+    w2_grads = np.transpose(relu(feature_vector))
+    return np.array([
+        w1_grads,
+        w2_grads
+    ])
+
 def get_qa_values(state):
     feature_vector = np.zeros((1, state_space_size))
     feature_vector[0][state] = 1
@@ -14,6 +24,11 @@ def get_qa_values(state):
 
 def choose_action(state):
     qa_values = get_qa_values(state)
+    
+    if np.random.randint() < 0.9:
+        return np.argmax(qa_values)
+    else:
+        return np.random.choice([0, 1])
     
 
 if __name__ == "__main__":
@@ -32,8 +47,40 @@ if __name__ == "__main__":
     
     for n in range(num_of_episodes):
         done = False
-        state = int(state_space_size)
-        action = 
+        state = int(state_space_size / 2)
+        action = choose_action(state)
+        state_action_value = get_qa_values(state)[action]
+        step_size = 0.001
+        gamma = 0.5
         
         while not done:
+            # take action, observe reward
+            if action == 0:
+                new_state = state - 1
+            else:
+                new_state = state + action
+            reward = 0
+            if new_state == state_space_size - 1:
+                reward = 10
+                done = True
+            elif new_state == 0:
+                reward = -10
+                done = True
             
+            if done:
+                td_error = reward + gamma * state_action_value
+                grads = get_grads(state, action)
+                weights[0]["W"] += step_size * td_error * grads[0]
+                weights[1]["W"][:, action] += step_size * td_error * grads[1]
+                break
+            
+            new_action = choose_action(new_state)
+            new_state_action_value = get_qa_values(new_state)[new_action]
+            td_error = reward + gamma * new_state_action_value - state_action_value
+            grads = get_grads(state, action)
+            weights[0]["W"] += step_size * td_error * grads[0]
+            weights[1]["W"][:, action] += step_size * td_error * grads[1]
+            
+            state = new_state
+            action = new_action
+            state_action_value = new_state_action_value
